313185da79ae2b3229b9da5e191e2ef360115b86
mpannala12 <mpannala@andrew.cmu.edu>
2021-10-30 17:17:44 -0400

    Updated apps.py to use new features

--- dockerfile/apps/app.py
+++ dockerfile/apps/app.py
@@ -12,15 +12,36 @@ def hello():
 @app.route('/predict')
 def predict():
 	 #use entries from the query string here but could also use json
-     age = request.args.get('age')
-     absences = request.args.get('absences')
+     studytime = request.args.get('studytime')
+     failures = request.args.get('failures')
+     schoolsup = request.args.get('schoolsup')
+     famsup = request.args.get('famsup')
+     activities = request.args.get('activities')
+     freetime = request.args.get('freetime')
+     higher = request.args.get('higher')
+     Dalc = request.args.get('Dalc')
+     Walc = request.args.get('Walc')
+     internet = request.args.get('internet')
      health = request.args.get('health')
-     data = [[age],[health],[absences]]
-     query_df = pd.DataFrame({ 'age' : pd.Series(age) ,'health' : pd.Series(health) ,'absences' : pd.Series(absences)})
+     absences = request.args.get('absences')
+     data = [[studytime],[failures],[schoolsup],[famsup],[activities],[freetime],[higher],
+            [Dalc],[Walc],[internet],[health],[absences]]
+     query_df = pd.DataFrame({ 'studytime' : pd.Series(studytime),
+                                'failures' : pd.Series(failures),
+                                'schoolsup' : pd.Series(schoolsup), 
+                                'famsup' : pd.Series(famsup),
+                                'activities' : pd.Series(activities),
+                                'freetime' : pd.Series(freetime), 
+                                'higher' : pd.Series(higher),
+                                'Dalc' : pd.Series(Dalc),
+                                'Walc' : pd.Series(Walc), 
+                                'internet' : pd.Series(internet),
+                                'health' : pd.Series(health),
+                                'absences' : pd.Series(absences), })
      query = pd.get_dummies(query_df)
      prediction = clf.predict(query)
      return jsonify(np.asscalar(prediction))
 
 if __name__ == '__main__':
-    clf = joblib.load('/apps/model.pkl')
+    clf = joblib.load('/apps/model_new.pkl')
     app.run(host="0.0.0.0", debug=True)
