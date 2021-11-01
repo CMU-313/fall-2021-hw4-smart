469453451ff010a3af8597fc67e1c64bf49681fd
mpannala12 <mpannala@andrew.cmu.edu>
2021-11-01 13:59:34 -0400

    Updated app.py dataframe order to fix prediction bug

--- dockerfile/apps/app.py
+++ dockerfile/apps/app.py
@@ -11,35 +11,38 @@ def hello():
 
 @app.route('/predict')
 def predict():
-	 #use entries from the query string here but could also use json
-     studytime = request.args.get('studytime')
-     failures = request.args.get('failures')
-     schoolsup = request.args.get('schoolsup')
-     famsup = request.args.get('famsup')
-     activities = request.args.get('activities')
-     freetime = request.args.get('freetime')
-     higher = request.args.get('higher')
-     Dalc = request.args.get('Dalc')
-     Walc = request.args.get('Walc')
-     internet = request.args.get('internet')
-     health = request.args.get('health')
-     absences = request.args.get('absences')
-     data = [[studytime],[failures],[schoolsup],[famsup],[activities],[freetime],[higher],
-            [Dalc],[Walc],[internet],[health],[absences]]
+	 # parse entries from the input
+     studytime = request.args.get('studytime', type=int)
+     failures = request.args.get('failures', type=int)
+     schoolsup = request.args.get('schoolsup', type=int)
+     famsup = request.args.get('famsup', type=int)
+     activities = request.args.get('activities', type=int)
+     freetime = request.args.get('freetime', type=int)
+     higher = request.args.get('higher', type=int)
+     Dalc = request.args.get('Dalc', type=int)
+     Walc = request.args.get('Walc', type=int)
+     internet = request.args.get('internet', type=int)
+     health = request.args.get('health', type=int)
+     absences = request.args.get('absences', type=int)
+
+     # produce dataframe
      query_df = pd.DataFrame({ 'studytime' : pd.Series(studytime),
                                 'failures' : pd.Series(failures),
                                 'schoolsup' : pd.Series(schoolsup), 
                                 'famsup' : pd.Series(famsup),
                                 'activities' : pd.Series(activities),
-                                'freetime' : pd.Series(freetime), 
                                 'higher' : pd.Series(higher),
+                                'internet' : pd.Series(internet),
+                                'freetime' : pd.Series(freetime), 
                                 'Dalc' : pd.Series(Dalc),
                                 'Walc' : pd.Series(Walc), 
-                                'internet' : pd.Series(internet),
                                 'health' : pd.Series(health),
-                                'absences' : pd.Series(absences), })
+                                'absences' : pd.Series(absences),
+                                })
      query = pd.get_dummies(query_df)
      prediction = clf.predict(query)
+
+     # return prediction
      return jsonify(np.asscalar(prediction))
 
 if __name__ == '__main__':
